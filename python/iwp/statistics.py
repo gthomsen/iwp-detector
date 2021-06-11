71842fc59703de448679ab99361d70776b5e8954
Greg Thomsen <gthomsen@gmail.com>
2021-06-11 16:52:52 -0400

    Topic: IWP Statistics Module Saving and Loading Support Update
    Description:
      * Added support for saving and loading variable statistics serialized into
        JSON.  This is required for saving computed statistics between executions
        and is a minimal interface for now.

--- python/iwp/statistics.py
+++ python/iwp/statistics.py
@@ -1,3 +1,5 @@
+import json
+
 # XXX: create a class to wrap statistics.  methods/attributes for each of the
 #      the descriptive variables.  method to load and save stats?
 
@@ -34,3 +36,50 @@ def compute_statistics( dataset ):
                       task_std.compute().values)
 
     return (variable_min, variable_max, variable_std)
+
+def save_statistics( statistics_path, statistics, pretty_flag=True ):
+    """
+    Serializes a statistics dictionary to disk as JSON.
+
+    Takes 3 arguments:
+
+      statistics_path - Path to save the statistics to.
+      statistics      - Dictionary of statistics to save.  Keys are variable
+                        names and the values are dictionaries containing
+                        the underlying statistics.
+      pretty_flag     - Optional flag specifying whether the serialized JSON is
+                        pretty-printed or not.  If omitted, defaults to True.
+
+    Returns nothing.
+
+    """
+
+    with open( statistics_path, "w" ) as statistics_fp:
+        #
+        # NOTE: we sort the dictionary keys so that it is easier to compare
+        #       different labels without custom tools.
+        #
+        json.dump( statistics, statistics_fp, indent=4, sort_keys=True )
+
+    return
+
+def load_statistics( statistics_path ):
+    """
+    Loads statistics from an on-disk JSON file.
+
+    Takes 1 argument:
+
+      statistics_path - Path to load statistics from.
+
+    Returns 1 value:
+
+      statistics - Dictionary of the statistics loaded.  Keys are variable names
+                   and the values are dictionaries containing the underlying
+                   statistics.
+
+    """
+
+    with open( statistics_path, "r" ) as statistics_fp:
+        statistics = json.load( statistics_fp )
+
+    return statistics
